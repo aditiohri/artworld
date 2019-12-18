fbd65071a65a6bd5a61e7f75b858b507a1ceb14f
Aditi Ohri <didiohri@gmail.com>
2019-12-18 17:56:47 -0600

    accept pull request and edit code from fabian

--- artworld/artworld/settings.py
+++ artworld/artworld/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'artworld.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'artwork',
+        'NAME': 'artworld',
     }
 }
 
--- artworld/main_app/models.py
+++ artworld/main_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Art(models.model):
+class Art(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     year = models.IntegerField()
@@ -12,25 +12,25 @@ class Art(models.model):
     def __str__(self):
         return f"{self.title} {self.artist}"
 
-class Customer(models.model):
+class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
 
-class Order(models.model):
-    customer = models.ForeignKey(Customer, on_delete=CASCADE)
-    art = models.ForeignKey(Art, on_delete=CASCADE)
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    art = models.ForeignKey(Art, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
     def __str__(self):
         return self.customer
 
-class Card(models.model):
+class Card(models.Model):
     name = models.CharField(max_length=100)
     number = models.IntegerField()
     expiry = models.DateField()
-    cvv = models.IntergerField()
+    cvv = models.IntegerField()
     billAddress = models.CharField(max_length=100)
 
     def __str__(self):
