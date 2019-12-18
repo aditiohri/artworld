74b1929801e09707b29e6a2b7650595c8e07b288
Fabian Acosta <acosahf@hotmail.com>
2019-12-18 17:14:56 -0600

    added the art model

--- artworld/main_app/models.py
+++ artworld/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Art(models.model):
+    title = models.charField(max_length=100)
+    description = models.TextField(max_length=250)
+    year = models.IntegerField()
+    price = models.IntegerField()
+    artist = models.charField(max_length=100)
+
+    def __str__(self):
+        return f"{self.title} {self.artist}"
