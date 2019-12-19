60852b6f8a62ec99aff55028e77d4b885ad2c845
Fabian Acosta <acosahf@hotmail.com>
2019-12-19 16:39:09 -0600

    pair programmed with Dinny to create Artlist

--- artworld/main_app/views.py
+++ artworld/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Art
 
 # Create your views here.
@@ -7,3 +8,9 @@ from .models import Art
 def arts_index(request):
     arts = Art.objects.all()
     return render(request, 'index.html', { 'arts': arts })
+
+class ArtList(ListView):
+    model = Art
+    fields = '__all__'
+    success_url = '/art/'
+    template_name = 'art.html'
