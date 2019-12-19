de024b186c3d2fbbbd43286fcabbe2afbe7ecbe3
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-19 12:55:07 -0600

    create art_index view

--- artworld/main_app/views.py
+++ artworld/main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Art
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Art World Index")
+
+def arts_index(resquest):
+    arts = Art.objects.all()
+  return render(request, 'arts/index.html', { 'arts': arts })
