9b5c3750af4f4f0184a7a2117849d755f3825f06
Aditi Ohri <didiohri@gmail.com>
2019-12-19 17:55:17 -0600

    add auth, pair programming with team

--- artworld/main_app/models.py
+++ artworld/main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 # Create your models here.
 
@@ -13,6 +15,9 @@ class Art(models.Model):
 
     def __str__(self):
         return f"{self.title} {self.artist}"
+    
+    def get_absolute_url(self):
+        return reverse('', kwargs={'art_id': self.id})
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
--- artworld/main_app/views.py
+++ artworld/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import ListView
 from django.contrib.auth import login
