1f085a14b565982fc6a9a1136b07d71b4deb39fd
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-20 15:20:16 -0600

    create a route for arts_detail

--- artworld/main_app/urls.py
+++ artworld/main_app/urls.py
@@ -4,6 +4,7 @@ from main_app.views import ArtList
 
 urlpatterns = [
     path('', views.arts_index, name='index'),
-    path('art/', ArtList.as_view()),
+    # path('art/', ArtList.as_view()),
     path('accounts/signup/', views.signup, name='signup'),
+    path('arts/<int:art_id>/', views.arts_detail, name='detail'),
 ]
