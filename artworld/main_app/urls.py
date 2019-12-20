7c93f18d15ec14daef3884d85793912872d4f7a2
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-20 17:26:11 -0600

    create routes for orders index, detail and add(create a new order

--- artworld/main_app/urls.py
+++ artworld/main_app/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.arts_index, name='index'),
     # path('art/', ArtList.as_view()),
     path('accounts/signup/', views.signup, name='signup'),
-    path('arts/<int:art_id>/', views.arts_detail, name='detail'),
+    path('arts/<int:art_id>/', views.art_detail, name='detail'),
+    path('orders/', views.orders_index, name='order_index'),
+    path('orders/<int:art_id>/', views.order_detail, name='order_detail'),
+    path('orders/<int:order_id>/add_order/', views.add_order, name='add_order'),
 ]
