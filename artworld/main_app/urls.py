9139c79ad05ee0b042c0548196bdbcfae9ee335a
Aditi Ohri <didiohri@gmail.com>
2019-12-20 15:41:49 -0600

    silence import ArtList in main_app/urls.py after accepting Dinny's pull request

--- artworld/main_app/urls.py
+++ artworld/main_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from main_app.views import ArtList
+# from main_app.views import ArtList
 
 urlpatterns = [
     path('', views.arts_index, name='index'),
