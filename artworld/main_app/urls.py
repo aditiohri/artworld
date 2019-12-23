1a25e22f1d51a2affb131c40cbec5eb9fbfd442e
Aditi Ohri <didiohri@gmail.com>
2019-12-23 17:59:23 -0600

    update path for add_order

--- artworld/main_app/urls.py
+++ artworld/main_app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('arts/<int:art_id>/', views.art_detail, name='detail'),
     path('orders/', views.orders_index, name='order_index'),
     path('orders/<int:art_id>/', views.order_detail, name='order_detail'),
-    path('orders/<int:order_id>/add_order/', views.add_order, name='add_order'),
+    path('arts/<int:art_id>/add_order/', views.add_order, name='add_order'),
 ]
