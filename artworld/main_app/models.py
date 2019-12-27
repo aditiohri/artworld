113836498b1099d98646eaa74c4e98f2185a4ec8
Fabian Acosta <acosahf@hotmail.com>
2019-12-27 16:58:09 -0600

    refactor order to cartitem and created a new model called order

--- artworld/main_app/models.py
+++ artworld/main_app/models.py
@@ -11,17 +11,44 @@ class Art(models.Model):
     year = models.IntegerField()
     price = models.IntegerField()
     artist = models.CharField(max_length=100)
+    image = models.ImageField()
 
     def __str__(self):
         return f"{self.title} {self.artist}"
 
-class Order(models.Model):
+class CartItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     art = models.ForeignKey(Art, on_delete=models.CASCADE)
-    address = models.CharField(max_length=100)
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return self.art
+
+    def get_total_item_price(self):
+        return self.item.price
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    items = models.ManyToManyField(CartItem)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    ordered = models.BooleanField(default=False)
+    shipping_address = models.ForeignKey(
+        'Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
+    billing_address = models.ForeignKey(
+        'Address', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
+    payment = models.ForeignKey(
+        'Payment', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_total(self):
+        total = 0
+        for cart_item in self.items.all():
+            total += cart_item.get_final_price()
+        return total
+
+
     
 class Card(models.Model):
     name = models.CharField(max_length=100)
