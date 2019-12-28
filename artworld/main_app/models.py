0fe35f9b1d7d6eedea2bd1cb56ecea98433a1706
Aditi Ohri <didiohri@gmail.com>
2019-12-27 18:48:45 -0600

    add address and payment models

--- artworld/main_app/models.py
+++ artworld/main_app/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.conf import settings
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+ADDRESS_CHOICES = (
+    ('B', 'Billing'),
+    ('S', 'Shipping'),
+)
 
 class Art(models.Model):
     title = models.CharField(max_length=100)
@@ -48,6 +54,29 @@ class Order(models.Model):
             total += cart_item.get_final_price()
         return total
 
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    country = CountryField(multiple=False)
+    zip = models.CharField(max_length=100)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+
+class Payment(models.Model):
+    # stripe_charge_id = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
 
     
 class Card(models.Model):
