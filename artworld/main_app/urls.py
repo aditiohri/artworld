58e6498876d1346340d136900deb297a1e8e5d02
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-30 16:30:42 -0600

    create route for CheckoutView

--- artworld/main_app/urls.py
+++ artworld/main_app/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('cart/<int:art_id>/add_cart/', views.add_cart, name='add_cart'), 
     path('cart/delete', views.delete_cart, name='delete_cart'),
     path('cart/<int:art_id>/delete', views.delete_cart_item, name='delete_cart_item'),
-    path('checkout/', views.checkout, name='checkout')
+    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
 ]
