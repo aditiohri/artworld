160f9b245bff06ce3ebb0058266837f87ca6fabc
Aditi Ohri <didiohri@gmail.com>
2019-12-30 19:45:41 -0600

    pair programming with pips: silence payment option for now

--- artworld/main_app/forms.py
+++ artworld/main_app/forms.py
@@ -32,5 +32,5 @@ class CheckoutForm(forms.Form):
     set_default_billing = forms.BooleanField(required=False)
     use_default_billing = forms.BooleanField(required=False)
 
-    payment_option = forms.ChoiceField(
-        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+    # payment_option = forms.ChoiceField(
+    #     widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
