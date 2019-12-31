93ba2d6ee14236542815cbed96e3cd92d8dcbb56
Aditi Ohri <didiohri@gmail.com>
2019-12-30 19:44:31 -0600

    import address and payment models to admin

--- artworld/main_app/admin.py
+++ artworld/main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Art, Cart, Order
+from .models import Art, Cart, Order, Address, Payment
 
 # Register your models here.
 admin.site.register(Art)
 admin.site.register(Cart)
 admin.site.register(Order)
+admin.site.register(Address)
+admin.site.register(Payment)
