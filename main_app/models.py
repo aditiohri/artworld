df1caeb91224c9cc3b01dbc616cfae7442bc16a8
Aditi Ohri <didiohri@gmail.com>
2020-01-02 14:29:05 -0600

    import stripe, os and stripe key

--- main_app/models.py
+++ main_app/models.py
@@ -76,7 +76,7 @@ class Address(models.Model):
         verbose_name_plural = 'Addresses'
 
 class Payment(models.Model):
-    # stripe_charge_id = models.CharField(max_length=50)
+    stripe_charge_id = models.CharField(max_length=50)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
     amount = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
