dd01dc88045d89cdc1603add37bde959efe90455
Aditi Ohri <didiohri@gmail.com>
2020-01-02 18:34:10 -0600

    remove one-click purchasing from userprofile

--- main_app/models.py
+++ main_app/models.py
@@ -12,10 +12,10 @@ ADDRESS_CHOICES = (
 )
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
-    one_click_purchasing = models.BooleanField(default=False)
+    # one_click_purchasing = models.BooleanField(default=False)
+
     def __str__(self):
         return self.user.username
 
